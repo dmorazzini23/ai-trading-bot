977e025cd7aca13ef141f024a150f2499a5a756f
dmorazzini23 <dmorazzini23@gmail.com>
2025-06-05 08:23:01 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1174,7 +1174,7 @@ SECRET_KEY = APCA_API_SECRET_KEY
 BASE_URL = ALPACA_BASE_URL
 paper = ALPACA_PAPER
 trading_client = TradingClient(API_KEY, SECRET_KEY, paper=paper)
-data_client = StockHistoricalDataClient(API_KEY, SECRET_KEY, paper=paper)
+data_client = StockHistoricalDataClient(API_KEY, SECRET_KEY)
 ctx = BotContext(
     api=trading_client,
     data_client=data_client,
