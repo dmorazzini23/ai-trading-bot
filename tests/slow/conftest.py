4e4695d60f5e2a663fb92a2d8216c64ec131353d
dmorazzini23 <dmorazzini23@gmail.com>
2025-06-27 20:23:23 -0700

    Update pytest configuration and requirements

--- tests/slow/conftest.py
+++ tests/slow/conftest.py
@@ -1,22 +1,8 @@
-import pytest
+def reload_module(module):
+    print(f"Stub reload_module called for: {module}")
+    return module
 
 
-def pytest_addoption(parser):
-    parser.addoption("--runslow", action="store_true", help="Run slow tests")
-
-
-def pytest_configure(config):
-    config.addinivalue_line("markers", "slow: mark test as slow")
-
-
-def pytest_collection_modifyitems(config, items):
-    try:
-        if config.getoption("--runslow"):
-            return
-    except ValueError:
-        # Option not registered when this conftest is loaded
-        pass
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
+def load_runner():
+    print("Stub load_runner")
+    return None
