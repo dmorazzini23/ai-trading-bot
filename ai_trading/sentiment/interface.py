8ac13eac00f78e78b1ee58da482f09b5c5a8dcba
Dominic Morazzini <dmorazzini23@gmail.com>
2026-03-07 12:39:26 -0800

    Mod (210)

--- ai_trading/sentiment/interface.py
+++
@@ -1,7 +0,0 @@
-"""Sentiment interface backed by :mod:`ai_trading.analysis.sentiment`."""
-from __future__ import annotations
-
-from ai_trading.analysis.sentiment import analyze_text
-
-
-__all__ = ["analyze_text"]
